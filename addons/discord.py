a0534129d689fc00f2eaf99ba0cca2d68e84d15e
Leko <rexx0520@gmail.com>
2021-03-18 20:57:35 +0800

    fix: discord webhook

--- addons/discord.py
+++ addons/discord.py
@@ -8,6 +8,7 @@ def send(webhook_url, message):
 
     req = urllib.request.Request(webhook_url, method="POST")
     req.add_header('Content-Type', 'application/json')
+    req.add_header('User-Agent', 'Auto YTArchive Raw 1.0')
     payload = json.dumps(payload).encode()
 
     return urllib.request.urlopen(req, data=payload)
