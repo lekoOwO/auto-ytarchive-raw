e02fb4978a31d9f8225f0a0793e5609e439e4309
showgan <rexx0520@gmail.com>
2021-03-28 14:25:02 +0800

    feat: support telegram file album

--- addons/telegram.py
+++ addons/telegram.py
@@ -20,26 +20,43 @@ def send(token, chat_id, message):
     return urllib.request.urlopen(req, data=payload)
 
 def send_files(token, chat_id, message, files):
+    if len(files) > 1:
+        return send_multi_files(token, chat_id, message, files)
+
     payload = [
         ("chat_id", chat_id),
         ("caption", message),
         ("parse_mode", "markdown")
     ]
 
-    if len(files) > 1:
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            zip_path = os.path.join(tmp_dir, "archive.zip")
-            with ZipFile(zip_path, 'w') as zip_obj:
-                for x in files:
-                    zip_obj.write(x)
-            payload.append(("document", f"f'{zip_path}'"))
-
-            content_type, payload = utils.encode_multipart_formdata(payload)
-    else:
-        payload.append(("document", f"f'{files[0]}'"))
-        content_type, payload = utils.encode_multipart_formdata(payload)
+    payload.append(("document", f"f'{files[0]}'"))
+    content_type, payload = utils.encode_multipart_formdata(payload)
 
     req = urllib.request.Request(f"https://api.telegram.org/bot{token}/sendDocument", method="POST")
+    req.add_header('Content-Type', content_type)
+
+    return urllib.request.urlopen(req, data=payload)
+
+
+def send_multi_files(token, chat_id, message, files):
+    payload = [
+        ("chat_id", chat_id),
+        ("allow_sending_without_reply", "true")
+    ]
+
+    media = []
+    for i in range(len(files)):
+        payload.append((f"file{i}", f"f'{files[i]}'"))
+        media.append({
+            "type": "document",
+            "media": f"attach://file{i}",
+            "caption": message if i == (len(files) - 1) else "",
+            "parse_mode": "markdown"
+        })
+    
+    payload.append(("media", json.dumps(media)))
+    content_type, payload = utils.encode_multipart_formdata(payload)
+    req = urllib.request.Request(f"https://api.telegram.org/bot{token}/sendMediaGroup", method="POST")
     req.add_header('Content-Type', content_type)
 
     return urllib.request.urlopen(req, data=payload)
