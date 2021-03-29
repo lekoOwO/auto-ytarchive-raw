94a42d3ee296a66cacc7e2d45b97e5a045263d8c
Leko <rexx0520@gmail.com>
2021-03-30 03:27:41 +0800

    fix: fcm module

--- addons/fcm.py
+++ addons/fcm.py
@@ -7,18 +7,24 @@ import urllib.request, urllib.parse
 
 def onlive(video_id, channel_id, channel_name):
     data = {
-        'title': text.FCM_TITLE.format(video_id=video_id, channel_name=channel_name, channel_id=channel_id),
-        "message": text.FCM_MESSAGE.format(video_id=video_id, channel_name=channel_name, channel_id=channel_id),
-        "click_action": f"https://youtu.be/{video_id}"
+        "to": "/topics/all",
+        "validateOnly": False,
+        "notification": {
+            'title': text.FCM_TITLE.format(video_id=video_id, channel_name=channel_name, channel_id=channel_id),
+            "body": text.FCM_MESSAGE.format(video_id=video_id, channel_name=channel_name, channel_id=channel_id),
+            "click_action": f"https://youtu.be/{video_id}"
+        }
     }
+
     if const.FCM_ICON:
-        data["icon"] = const.FCM_ICON
+        data["notification"]["icon"] = const.FCM_ICON
 
     data = json.dumps(data).encode()
 
-    req = urllib.request.Request(url="https://fcm.googleapis.com/fcm/send", method="POST", data=data)
+    req = urllib.request.Request(
+        url="https://fcm.googleapis.com/fcm/send", method="POST", data=data)
     req.add_header('Content-Type', 'application/json')
-    
+
     req.add_header("Authorization", f"key={const.FCM_API_KEY}")
 
     try:
